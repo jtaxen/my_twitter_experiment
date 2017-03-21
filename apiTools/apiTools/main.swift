5e292042020be81d28def90201d0e9fdf99536ca
ÅF Jacob Taxén <af@AF-Apple1s-MacBook-Pro.local>
2017-03-21 16:56:28 +0100

    A script written in Python added to work as a bridge between Python and Swift. In this way, we get the best of both worlds, both the serpentine and the aviating

---
+++ apiTools/apiTools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  apiTools
+//
+//  Created by ÅF Jacob Taxén on 2017-03-21.
+//  Copyright © 2017 Jazze and the Tazzers. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
